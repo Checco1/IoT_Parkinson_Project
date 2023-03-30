014640ed8a1449cde5921031273c7aea6da66679
martagood98 <120021371+martagood98@users.noreply.github.com>
2023-03-30 14:05:09 +0200

    ID fixed

--- FINAL_PROJECT/Simulated_Patients/Patient1/info_provider.py
+++ FINAL_PROJECT/Simulated_Patients/Patient1/info_provider.py
@@ -11,6 +11,7 @@ class InfoProvider():
     def GET(self,*uri):
         self.uri = uri
         self.topics={}
+        found=0
 
         #Empty uri --> Error
         if len(uri)==0:
@@ -21,9 +22,11 @@ class InfoProvider():
             if uri[0]=="get_topics" and uri[1]=="sensor" and "patient" in uri[2]:
                 patient=uri[2]
                 deviceType = uri[1]
-                patientID=patient.replace("patient","")
+                patient_number=patient.replace("patient","")
+                patientID = "p_"+str(patient_number)
+                print(patient_number, patientID)
                 for p in self.patient:
-                    if int(p["patientID"])==int(patientID):
+                    if p["patientID"]==patientID:
                         for device in p["deviceList"]:
                             if device["deviceType"] == deviceType:
                                 for service in device["Services"]:
@@ -32,27 +35,54 @@ class InfoProvider():
                         print(json.dumps(self.topics))
                         return json.dumps(self.topics)
                     
-            #uri type for actuators's topics --> localhost:8080/get_topics/actuator/patient1
+            #uri type for actuators's topics 
+            # --> localhost:8080/get_topics/actuator/patient1/<name_actuator>
             elif uri[0]=="get_topics" and uri[1]=="actuator" and "patient" in uri[2]:
-                patient=uri[2]
-                deviceType = uri[1]
-                patientID=patient.replace("patient","")
-                for p in self.patient:
-                    if int(p["patientID"])==int(patientID):
-                        for device in p["deviceList"]:
-                            if device["deviceType"] == deviceType:
-                                for service in device["Services"]:
-                                    if service["serviceType"] == "MQTT":
-                                        self.topics.update({str(device["deviceID"]):str(service["topic"])})
-                        print(json.dumps(self.topics))
-                        return json.dumps(self.topics)
+                if len(uri)==3:
+                    raise cherrypy.HTTPError(404,"Missing actuator: please, retry entering the name of the actuator to get its topic")
+                else:
+                    patient=uri[2]
+                    deviceType = uri[1]
+                    patient_number=patient.replace("patient","")
+                    patientID = "p_"+str(patient_number)
+                    deviceName = uri[3]+str(patient_number)
+                    
+                    for p in self.patient:
+                        if p["patientID"]==patientID:
+                            if "dbs" in deviceName or "soundfeedback" in deviceName:
+                                for device in p["deviceList"]:
+                                    if device["deviceType"] == deviceType and device["deviceID"]==(deviceName):
+                                        for service in device["Services"]:
+                                            if service["serviceType"] == "MQTT":
+                                                self.topics.update({str(device["deviceID"]):str(service["topic"])})
+                                                found=1
+                                if found == 0:
+                                    raise cherrypy.HTTPError(
+                                                                status=404,
+                                                                message=f"Actuator {deviceName} not found. Please retry entering the following\
+                                                                actuators' name: dbs, soundfeedback, TeleBot or ThingSpeak"
+                                                            )
+                            elif "TeleBot" in deviceName or "ThingSpeak" in deviceName:
+                                for service in p["Statistic_services"]:
+                                    if service["ServiceName"] in deviceName:
+                                        self.topics.update({str(service["ServiceName"]):str(service["topic"])})
+                                        found = 1
+                                if found == 0:
+                                    raise cherrypy.HTTPError(
+                                                                status=404,
+                                                                message=f"Actuator {deviceName} not found. Please retry entering the following\
+                                                                actuators' name: dbs, soundfeedback, TeleBot or ThingSpeak"
+                                                            )
+                            print(json.dumps(self.topics))
+                            return json.dumps(self.topics)
                     
             #uri type for telebot and ThingSpeak adaptor topics --> localhost:8080/get_topics/Statistic_services/patient1
             elif uri[0]=="get_topics" and uri[1]=="Statistic_services" and "patient" in uri[2]:
                 patient=uri[2]
-                patientID=patient.replace("patient","")
+                patient_number=patient.replace("patient","")
+                patientID = "p_"+str(patient_number)
                 for p in self.patient:
-                    if int(p["patientID"])==int(patientID):
+                    if p["patientID"]==patientID:
                         for service in p["Statistic_services"]:
                             self.topics.update({str(service["ServiceName"]):str(service["topic"])})
                         print(json.dumps(self.topics))
