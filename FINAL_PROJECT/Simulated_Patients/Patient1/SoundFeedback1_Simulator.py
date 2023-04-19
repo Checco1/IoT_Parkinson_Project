a31d874b4537b2be63694b3eca570bf57a02ac50
martagood98 <120021371+martagood98@users.noreply.github.com>
2023-04-19 16:49:01 +0200

    actuators update

--- FINAL_PROJECT/Simulated_Patients/Patient1/SoundFeedback1_Simulator.py
+++ FINAL_PROJECT/Simulated_Patients/Patient1/SoundFeedback1_Simulator.py
@@ -23,15 +23,15 @@ class RetrievePatientInfo():
         self.url = url
 
     def GetTopic(self,patientID): #localhost:8080/info/p_1
-        self.topics={}
         request=self.url+"/info/p_"+str(patientID)
         response = requests.get(request)
         response_json=response.json()
         for device in response_json["devices"]:
-            if device["deviceType"]=="actuator":
+            if device["deviceID"]=="soundfeedback1":
                 for service in device["Services"]:
                     if service["serviceType"] == "MQTT":
-                        self.topics.update({str(device["deviceID"]):str(service["topic"])})
+                        self.topics= {"activation":str(service["topic"]["activation"]),
+                                      "update_check": str(service["topic"]["update_check"])}
                 
         return(self.topics)
     
@@ -41,11 +41,25 @@ class RetrievePatientInfo():
         return(response.json())
 
 class SFSimulator():
-    def __init__(self, dbsID, topic, broker, port):
+    def __init__(self, dbsID, topic_activation, topic_update, broker, port):
         self.dbsID = dbsID
-        self.topic=topic
-        self.client = MyMQTT("SoundFeedback1",broker,port,self)
+        self.topic_activation=topic_activation
+        self.topic_update=topic_update
+        self.client = MyMQTT("SF1",broker,port,self)
         self.sf_activation = False
+        self.sf_timeUpdate = 0
+        self.message={"bn": "sf1_update",
+                "e":
+                    [
+                        {
+                            "measureType":"Activation",
+                            "unit":"bool",
+                            "timeStamp":"",
+                            "value":"True"
+                        }
+                    ]
+
+        }
 
     def notify(self, topic, msg):
         d = json.loads(msg)
@@ -57,6 +71,13 @@ class SFSimulator():
             print(f"The microservice has started the activation with the topic {topic}")
             self.t_activation=time.time()
 
+    def Update(self):
+        self.message["e"][0]["timeStamp"]=time.time()
+        print(self.topic_update)
+        print(self.message)
+        self.client.myPublish(self.topic_update,self.message)
+        print("Update published!")
+
 if __name__ == "__main__":
 
     #Initialization
@@ -65,17 +86,26 @@ if __name__ == "__main__":
     #Retrieve MQTT info (topics and settings) from patient.json
     patientID = 1
     info=RetrievePatientInfo("http://localhost:8080")
-    topic=info.GetTopic(patientID)["soundfeedback1"]
+    topic=info.GetTopic(patientID)
+    topic_activation = topic["activation"]
+    topic_update = topic["update_check"]
     settings=info.GetSettings()
     broker = settings["IP"]
     port = int(settings["mqtt_port"])
 
     #Start the simulator
-    soundfeedback1=SFSimulator("soundfeedback1", topic, broker, port)
+    soundfeedback1=SFSimulator("soundfeedback1",  topic_activation, topic_update, broker, port)
     soundfeedback1.client.start()
-    soundfeedback1.client.mySubscribe(topic)
+    soundfeedback1.client.mySubscribe(topic_activation)
+    start=time.time()
+
 
     while True:
+        soundfeedback1.sf_timeUpdate=time.time()-start
+        if soundfeedback1.sf_timeUpdate>250:
+            soundfeedback1.Update()
+            start=time.time()
+
         if soundfeedback1.sf_activation==True:
             if (soundfeedback1.t_activation-time.time())>10:
                 soundfeedback1.sf_activation=False
