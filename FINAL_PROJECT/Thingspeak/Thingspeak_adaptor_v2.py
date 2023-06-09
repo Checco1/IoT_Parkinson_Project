49c960b0d8d4c04ca7d955c2ee5e1d40f6e30d60
LucaBar8 <120021438+LucaBar8@users.noreply.github.com>
2023-06-09 16:53:30 +0200

    Thingspeak update

--- FINAL_PROJECT/Thingspeak/Thingspeak_adaptor_v2.py
+++ FINAL_PROJECT/Thingspeak/Thingspeak_adaptor_v2.py
@@ -1,62 +1,31 @@
+
 import json
-import paho.mqtt.client as PahoMqtt
+import threading
 import time
 import datetime
-import threading
 import requests
-import cherrypy
 from pathlib import Path
+from paho.mqtt.client import Client as MqttClient
+
+
 P = Path(__file__).parent.absolute()
-loop_flag = 1
-time_flag = 1
-CHERRY_CONF = str(P / "cherrypyconfing")
+
 FILE = P / "conf.json"
 
-def ts_publish(list,field,write_api,ts_url):
-    """Take a list of jsons and publish them via REST on ThingSpeak."""
-    field_reading={'api_key':str(write_api),'field'+ str(field):list}
-    ts_url
-    header_f={'Content_type':'application/json'}
-    request=requests.post(ts_url,field_reading,header_f)
-    request.close()
-
-def publish_flag(cnt,field,ts_url,write_api):
-    field_reading={'api_key':str(write_api),'field'+ str(field):cnt}
-    header_f={'Content_type':'application/json'}
-    request=requests.post(ts_url,field_reading,header_f)
-    request.close()
-
-def read_file(filename):
-    """Read json file to get catalogURL, port, topic and ThingSpeak URL."""
-    with open(filename, "r") as f:
-        data = json.loads(f.read())
-        url = data["ip"]
-        topic = data["baseTopic"]
-        port = data["port"]
-        ts_url = data["thingspeakURL"]
-        return (url, port, topic, ts_url)
-
-def Broker(url,port):
-    string="http://"+url+":"+port+"/broker"
-    broker=requests.get(string)
-    broker_ip=json.loads(broker.text)["IP"]
-    mqtt_port=json.loads(broker.text)["mqtt_port"]
-    return(broker_ip,mqtt_port)
-    
+loop_flag = 1
+time_flag = 1
 
 class Timer(threading.Thread):
     """15-second timer.
     It is used to prevent publishing on ThingSpeak too often.
     """
 
-    def __init__(self, ThreadID, name):
-        """Initialise thread widh ID and name."""
+    def __init__(self):
+        """Initialize the timer thread."""
         threading.Thread.__init__(self)
-        self.ThreadID = ThreadID
-        self.name = name
 
     def run(self):
-        """Run thread."""
+        """Run the timer thread."""
         global time_flag
         while True:
             time_flag = 1  # Start timer.
@@ -65,158 +34,155 @@ class Timer(threading.Thread):
             time.sleep(1)  # 1-sec cooldown.
 
 
-class MySubscriber(object):
+class MySubscriber():
     """MQTT subscriber."""
 
     def __init__(self, clientID, topic, broker_ip):
-        """Initialise MQTT client."""
+        """Initialize the MQTT subscriber."""
         self.clientID = clientID
         self.topic = topic
         self.messageBroker = broker_ip
-        self._paho_mqtt = PahoMqtt.Client(self.clientID,False)
+        self._paho_mqtt = MqttClient(self.clientID, False)
         self._paho_mqtt.on_connect = self.my_on_connect
         self._paho_mqtt.on_message = self.my_on_message_received
 
     def start(self):
-        """Start subscriber."""
-        self._paho_mqtt.connect(self.messageBroker,1883)
+        """Start the MQTT subscriber."""
+        self._paho_mqtt.connect(self.messageBroker, 1883)
         self._paho_mqtt.loop_start()
         self._paho_mqtt.subscribe(self.topic, 2)
         print("Subscribed to the topic: " + self.topic)
     
     def stop(self):
-        """Stop subscriber."""
+        """Stop the MQTT subscriber."""
         self._paho_mqtt.unsubscribe(self.topic)
         self._paho_mqtt.loop_stop()
         self._paho_mqtt.disconnect()
 
-    def my_on_connect(self, rc,clientID, topic, broker_ip):
-        #print("S - Connected to  - Result code:" (self.messageBroker,rc))
+    def my_on_connect(self, client, userdata, flags, rc):
+        """Handle MQTT connection event."""
         pass
-    def send_data(self):
-        """Take data from database, empty the database, return data."""
-        data = self.db.list_data
-        self.db.reset()
-        return data
 
-
-    def my_on_message_received(self, msg,clientID, broker_ip):
-        
-        (self.url, self.port, self.topic, self.ts_url) = read_file(FILE)
-        #read the message coming from the message_broker and upload data depending on the topic 
-        msg.payload = msg.payload.decode("utf-8")
-        message = json.loads(msg.payload)
+    def my_on_message_received(self, client, userdata, msg):
+        """Handle received MQTT message."""
+        (url, port, topic, ts_url) = read_file(FILE)
+        print(topic)
+        message = json.loads(msg.payload.decode("utf-8"))
         print(message)
-        Patient = message['bn']
-        print(Patient)
-        # Ask catalog for patientID from patient
-        #string = "http://" + self.url + ":" + self.port + "/info/" + Patient
-        #info_d = json.loads(requests.get(string).text)
-        #patientID = info_d["patientID"]
-
-        #thingspeakID="2055330"
-        write_api="3D509BA5PQ8SQ4EU"
-    
-        cont_tremor=0
-        cont_fall=0
-        cont_freezing=0
-        # Update values in the database.
+        Patient_topic = message['bn']
+        print(Patient_topic)
+        write_api = "3D509BA5PQ8SQ4EU"
+        cont_tremor = 0
+        cont_fall = 0
+        cont_freezing = 0
+        
+        #if Patient["e"][0]=="fall_manager":
         for i in message["e"]:
-            if i["n"] == 'alive':
+            if i["measureType"] == 'alive':
                 pass
             else:
-                topic = i["n"]
+                topic = i["measureType"]
                 if topic == "tremor_manager":
-                    value = i["v"]
+                    value = i["value"]
                     if value == 1:
-                        cont_tremor= cont_tremor+1
-                        field=4
-                        publish_flag(cont_tremor,field,self.ts_url,write_api)
+                        cont_tremor += 1
+                        field = 4
+                        publish_flag(cont_tremor, field, ts_url, write_api)
                 elif topic == "fall_manager":
-                    value = i["v"]
+                    value = i["value"]
                     if value == 1:
-                        cont_fall= cont_fall+1
-                        field=5
-                        publish_flag(cont_fall,field,self.ts_url,write_api)
+                        cont_fall += 1
+                        field = 5
+                        publish_flag(cont_fall, field, ts_url, write_api)
                 elif topic == "freezing_manager":
-                    value = i["v"]
+                    value = i["value"]
                     if value == 1:
-                        cont_freezing= cont_freezing+1
-                        field=6
-                        publish_flag(cont_freezing,field,self.ts_url,write_api)
-                elif topic=="WaistAccStats":
-                    value=i["v"]
+                        cont_freezing += 1
+                        field = 6
+                        publish_flag(cont_freezing, field, ts_url, write_api)
+                elif topic == "WaistAccStats":
+                    mean = i["value"]["mean"]
                     field=1
-                    ts_publish(value,field,write_api,self.ts_url)
-                elif topic=="WristAccStats":
-                    value=i["v"]
+                    ts_publish_m(mean, field, write_api, ts_url)
+                    std =i["value"]["std"]
                     field=2
-                    ts_publish(value,field,write_api,self.ts_url)
-                elif topic=="FeetPressureStats":
-                    value=i["v"]
+                    ts_publish_s(std, field, write_api, ts_url)
+                elif topic == "WristAccStats":
+                    mean = i["value"]["mean"]
+                    std =i["value"]["std"]
+                    field=7
+                    ts_publish_m(mean, field, write_api, ts_url)
+                    field = 8
+                    ts_publish_s(std, field, write_api, ts_url)
+                elif topic == "FeetPressureStats":
+                    mean = i["value"]["mean"]
                     field=3
-                    ts_publish(value,field,write_api,self.ts_url)
+                    ts_publish_m(mean, field, write_api, ts_url)
+                    std =i["value"]["std"]
+                    field = 9
+                    ts_publish_s(std, field, write_api, ts_url)
+
+
+def ts_publish_m(mean, field, write_api, ts_url): #publish the mean of the sensors
+    """Publish data via REST on ThingSpeak."""
+    field_reading = {'api_key': str(write_api), 'field' + str(field): mean}
+    headers_f = {'Content-Type': 'application/json'}
+    response = requests.post(ts_url,field_reading, headers_f)
+    response.close()
+
+def ts_publish_s(std, field, write_api, ts_url): #publish the standard deviation of the sensors
+    """Publish data via REST on ThingSpeak."""
+    field_reading = {'api_key': str(write_api), 'field' + str(field): std}
+    headers_f = {'Content-Type': 'application/json'}
+    response = requests.post(ts_url,field_reading, headers_f)
+    response.close()
+
+
+def publish_flag(cnt, field, ts_url, write_api):
+    """Publish flag count via REST on ThingSpeak."""
+    print("ciao")
+    field_reading = {'api_key': str(write_api), 'field' + str(field): cnt}
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(ts_url,field_reading, headers)
+    print(response)
+    response.close()
+
+
+def read_file(filename):
+    """Read JSON file to get catalogURL, port, topic, and ThingSpeak URL."""
+    with open(filename, "r") as f:
+        data = json.load(f)
+        url = data["ip"]
+        topic = data["baseTopic"]
+        port = data["port"]
+        ts_url = data["thingspeakURL"]
+        return url, port, topic, ts_url
 
 
 class Data(threading.Thread):
 
-    def __init__(self, ThreadID,name):
+    def __init__(self):
         threading.Thread.__init__(self)
-        self.ThreadID=ThreadID
-        self.name=name
         (self.url, self.port, self.topic, self.ts_url) = read_file(FILE)
-        (self.broker_ip,self.mqtt_port) = Broker(self.url,self.port)
+        (self.broker_ip, self.mqtt_port) = Broker(self.url, self.port)
 
     def run(self):
-        sub=MySubscriber("Thingspeak",self.topic,self.broker_ip)
-        sub.start()  
+        sub = MySubscriber("Thingspeak", self.topic, self.broker_ip)
+        sub.start()
 
-#restfull part to take data from thingspeak
 
-class CherryThread(threading.Thread):
-    """Thread to run CherryPy webserver."""
+def Broker(url, port):
+    broker_ip = "test.mosquitto.org"
+    mqtt_port = 1883
+    return broker_ip, mqtt_port
 
-    def __init__(self, ThreadID, name):
-        """Initialise thread widh ID and name."""
-        threading.Thread.__init__(self)
-        self.ThreadID = ThreadID
-        self.name = name
-        (self.url, self.port, self.topic, self.ts_url) = read_file(FILE)
-        (self.broker_ip, mqtt_port) = Broker(self.url, self.port)
-        self.mqtt_port = int(mqtt_port)
 
-    def run(self):
-        """Run thread."""
-        try:
-            cherrypy.tree.mount(WebServer(), '/', config=CHERRY_CONF)
-            cherrypy.config.update(CHERRY_CONF)
-            cherrypy.engine.start()
-            cherrypy.engine.block()
-        except KeyboardInterrupt:
-            print("Stopping the engine")
-            return
-
-class WebServer():
-    """CherryPy webserver.
-    It is used to take data from thingspeak and send them to the control strategies.
-    Ex: we want to know the statistics of patient 1 of latest 5 hours.
-    Adaptor takes info from thingspeak and send back to strategies a json.
-    """
-
-    exposed = True
-
-    @cherrypy.tools.json_out()
-    def GET(self, *uri, **params):
-        i=1 #to be implemented 
-
-def main ():
-    thread1 = Data(1, "Data")
+def main():
+    thread1 = Data()
     thread1.start()
-    thread2 = Timer(2, "Timer")
+    thread2 = Timer()
     thread2.start()
-    #therad3= CherryThread(3, "Cherrypy")
-    #therad3.start()
 
-if __name__== "__main__":
-    
+
+if __name__ == "__main__":
     main()
