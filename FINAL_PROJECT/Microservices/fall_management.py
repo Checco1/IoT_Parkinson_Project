8140fd448c148723d58a43f7b60dbb5b064a18cf
Candela <s306643@studenti.polito.it>
2023-06-19 19:22:27 +0200

    Improved microservices

--- FINAL_PROJECT/Microservices/fall_management.py
+++ FINAL_PROJECT/Microservices/fall_management.py
@@ -32,11 +32,12 @@ class fall_management():
                             "measureType":"fall_manager",
                             "unit":"bool",
                             "timeStamp":"",
-                            "value":"",
+                            "value":True,
                         }
                     ]
         }
         self.listOfPatients = [{"waistFlag" : 0, "pressureFlag": 0}]*512
+        self.sentFlag = ['OFF']*512
         self.sensor_id = "default"
         self.receivedPatientID = "default"
         self.receivedActuator = "default"
@@ -60,13 +61,12 @@ class fall_management():
         waistSensorName = "waist_acc" + str(patientNumber)
         pressureSensorName = "pressure" + str(patientNumber)
 
+        self.structure["bn"] = self.receivedPatientID + '/fall_manager'
         if(self.receivedActuator == waistSensorName):
             print(sensor_info)
             waist_freq = sensor_info["e"][0]["value"]
             self.structure["e"][0]["timeStamp"] = sensor_info["e"][0]["timeStamp"]
-            self.structure["e"][0]["value"] = 0
             if (waist_freq >= 1.5): #and (waist_freq <= 1.71): #must check if time last peak is > 1.5
-                self.structure["e"][0]["value"] = 1
                 print ("Stop at " + str(sensor_info["e"][0]["timeStamp"]) + "s")
                 self.listOfPatients[patientNumber]["waistFlag"] = 1
             else:
@@ -77,19 +77,29 @@ class fall_management():
             print(sensor_info)
             pressure = sensor_info["e"][0]["value"]
             self.structure["e"][0]["timeStamp"] = sensor_info["e"][0]["timeStamp"]
-            self.structure["e"][0]["value"] = 0
             if (pressure <= 30):
-                self.structure["e"][0]["value"] = 1
                 print ("Pressure lying at " + str(sensor_info["e"][0]["timeStamp"]) + "s")
                 self.listOfPatients[patientNumber]["pressureFlag"] = 1
             else:
                  self.listOfPatients[patientNumber]["pressureFlag"] = 0
+
+        if (self.sentFlag[patientNumber] == 'OFF'):
+            if (self.listOfPatients[patientNumber]["waistFlag"] == 1 and self.listOfPatients[patientNumber]["pressureFlag"] == 1):
+                self.sentFlag[patientNumber] = 'SEND_COMMAND'
+                print("Fall situation at " + str(sensor_info["e"][0]["timeStamp"]))
+                self.publisher(json.dumps(self.structure))
+                self.listOfPatients[patientNumber]["pressureFlag"] = 0
+                self.listOfPatients[patientNumber]["waistFlag"] = 0
+        elif(self.sentFlag[patientNumber] == 'SEND_COMAND'):
+            if (self.listOfPatients[patientNumber]["waistFlag"] == 1 or self.listOfPatients[patientNumber]["pressureFlag"] == 1):
+                self.sentFlag[patientNumber] = 'KEEPS_FALLING'
+            elif(self.listOfPatients[patientNumber]["waistFlag"] == 0 and self.listOfPatients[patientNumber]["pressureFlag"] == 0):
+                self.sentFlag[patientNumber] = 'OFF'
+        else:
+            if(self.listOfPatients[patientNumber]["waistFlag"] == 0 and self.listOfPatients[patientNumber]["pressureFlag"] == 0):
+                self.sentFlag[patientNumber] = 'OFF'
                  
-        if (self.listOfPatients[patientNumber]["waistFlag"] == 1 and self.listOfPatients[patientNumber]["pressureFlag"] == 1):
-            print("Fall situation at " + str(sensor_info["e"][0]["timeStamp"]))
-            self.publisher(json.dumps(self.structure))
-            self.listOfPatients[patientNumber]["pressureFlag"] = 0
-            self.listOfPatients[patientNumber]["waistFlag"] = 0
+
 
         return self.structure
 
@@ -99,12 +109,12 @@ class fall_management():
 
     def subscriber(self):
         self._paho_mqtt.subscribe(self.topic, 2)
-        print('Subscribed to' + self.topic)
+        print('Subscribed to ' + self.topic)
 
     def publisher(self, msg):
         topic = self.actuators_topic.replace( "PATIENT_ID", self.receivedPatientID)
         self._paho_mqtt.publish(topic, msg, 2)
-        print("published: " + str(msg) + " on " + self.actuators_topic)
+        print("published: " + str(msg) + " on " + topic)
 
     def stop(self):
         self._paho_mqtt.unsubscribe(self.topic)
@@ -143,7 +153,7 @@ if __name__ == "__main__":
     #=================ASK ABOUT TELEBOT'S URI=============
     #uri_actuators = 'http://localhost:8080/get_topics/Statistic_services/patient1'
     #actuators_topics= requests.get(uri_actuators).json()["TeleBot"]
-    actuators_topics = "ParkinsonHelper/PATIENT_ID/actuator/fall"
+    actuators_topics = "ParkinsonHelper/PATIENT_ID/microservices/fall"
     # Creating as many instances as clients, so they can comunicate with their corresponding actuator
     tm = fall_management(microserviceID, port, broker, sensor_topic, actuators_topics)
     tm.start()
